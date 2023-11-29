6b73d2c0ea50ed177462124919e2c98000c3a68c
7478f <valvalatan04@gmail.com>
2023-11-29 14:22:54 -0500

    fixed more css

--- project/Views/User/login.php
+++ project/Views/User/login.php
@@ -1,21 +1,39 @@
-<?php $this->render("Shared", "header", ['css' => 'loginPage'] ); ?>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="Styles/loginPage.css"/>
+    
+</head>
 <body>
-    <div class="login-container">
-        <?php if(!isset($user)) {?>
-        <form action="index.php?c=User&a=login" method="POST">
-            <h2>Login</h2>
-            <?php if (isset($error)) {
-                // echo '<p style="color: red;">' . $error . '</p>';
-            } ?>
-            <input type="text" name="username" placeholder="Username" required><br><br>
-            <input type="password" name="password" placeholder="Password" required><br><br>
-            <input type="submit" value="Login">
-        </form>
-        <?php } else {?>
-            <h2>Login</h2>
-            <?= "<p>You are already logged in " . $user['username'] . " living at " . $user['address'] . ".</p>" ?>
-        <?php }?>
+    <div class="nav-bar">
+        
+
     </div>
 
-<?php $this->render("Shared", "footer"); ?>
+
+    <div class="login-container">
+            <?php if(!isset($user)) {?>
+            <form action="index.php?c=User&a=login" method="POST">
+                <h2>Login</h2>
+                <?php if (isset($error)) {
+                    // echo '<p style="color: red;">' . $error . '</p>';
+                } ?>
+                <input type="text" name="username" placeholder="Username" required><br><br>
+                <input type="password" name="password" placeholder="Password" required><br><br>
+                <input type="submit" value="Login">
+            </form>
+            <?php } else {?>
+                <h2>Login</h2>
+                <?= "<p>You are already logged in " . $user['username'] . " living at " . $user['address'] . ".</p>" ?>
+            <?php }?>
+        </div>    
+
+
+</body>
+</html>
+
+
+
