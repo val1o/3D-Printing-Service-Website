8412e16dfd6b0bd8b8421bda12fb12a14a1f72ab
7478f <valvalatan04@gmail.com>
2023-12-01 00:46:07 -0500

    clean up css a bit

--- project/Views/User/login.php
+++ project/Views/User/login.php
@@ -1,6 +1,5 @@
 <?php $this->render("Shared", "header", ["css" => "loginPage", "title" => "Login Page"]); ?>
 
-<div class="page-container">
     <div class="login-container">
             <?php if(!isset($user)) {?>
             <form action="index.php?c=User&a=login" method="POST">
@@ -8,8 +7,11 @@
                 <?php if (isset($error)) {
                     // echo '<p style="color: red;">' . $error . '</p>';
                 } ?>
-                <input type="text" name="username" placeholder="Username" required><br><br>
-                <input type="password" name="password" placeholder="Password" required><br><br>
+                <label for="username"> Username: </label>
+                <input type="text" name="username" required><br><br>
+
+                <label for="password"> Password: </label>
+                <input type="password" name="password" required><br><br>
                 <input type="submit" value="Login">
             </form>
             <?php } else {?>
@@ -17,6 +19,5 @@
                 <?= "<p>You are already logged in " . $user['username'] . " living at " . $user['address'] . ".</p>" ?>
             <?php }?>
         </div>    
-    </div>
 
         <?php $this->render("Shared", "footer"); ?>
